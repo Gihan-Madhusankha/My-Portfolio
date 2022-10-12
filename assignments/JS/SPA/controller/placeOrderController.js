713f7f7a2876ca552ffb5745a836ab305460c6e4
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2022-10-12 21:21:48 +0530

    Set customer details by ID

--- assignments/JS/SPA/controller/placeOrderController.js
+++ assignments/JS/SPA/controller/placeOrderController.js
@@ -6,3 +6,23 @@ function loadAllCustomerIds() {
         $('#selectCusId').append(`<option>${cus.id}</option>`);
     }
 }
+
+
+$('#selectCusId').change(function () {
+    let selectedVal = $(this).val();
+    for (let cus of cusArray) {
+        if (cus.id == selectedVal) {
+            setCustomerDetails(cus);
+        } else if ('select' == selectedVal){
+            $('#cusID, #cusName, #cusAddress, #cusSalary').val("");
+        }
+    }
+
+});
+
+function setCustomerDetails(cusObj){
+    $('#cusID').val(cusObj.id);
+    $('#cusName').val(cusObj.name);
+    $('#cusAddress').val(cusObj.address);
+    $('#cusSalary').val(cusObj.salary);
+}
